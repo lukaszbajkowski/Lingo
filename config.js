b394880288eaf9ef0845371ad5005be37f4e1954
mixvert <bajkowski1e@gmail.com>
2023-11-21 23:55:41 +0100

    Deploy project to vercel

--- config.js
+++ config.js
@@ -2,3 +2,5 @@ module.exports = {
     port: process.env.PORT || 3001,
     database: process.env.DATABASE || 'mongodb://127.0.0.1:27017/lingo',
 }
+
+// mongodb+srv://bajkowski1e:3ehjPYbnDJ7blAJX@lingo.r2okvio.mongodb.net/?retryWrites=true&w=majority,
--- db/mongoose.js
+++ db/mongoose.js
@@ -1,8 +1,21 @@
-const mongoose = require("mongoose");
-const {database} = require("../config");
+// const mongoose = require("mongoose");
+// const {database} = require("../config");
+//
+// // db connection
+// mongoose.connect(database, {
+//     "useNewUrlParser": true,
+//     "useUnifiedTopology": true
+// });
 
-// db connection
-mongoose.connect(database, {
-    "useNewUrlParser": true,
-    "useUnifiedTopology": true
+const mongoose = require('mongoose');
+const dbUrl = process.env.DATABASE;
+
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
 });
+
+module.exports = mongoose;
