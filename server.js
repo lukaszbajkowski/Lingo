b600e398edd9146c90aa34455070177d777bb24e
mixvert <bajkowski1e@gmail.com>
2023-11-23 03:02:53 +0100

    More working on deploy project to vercel

--- server.js
+++ server.js
@@ -18,7 +18,4 @@ app.use(cors());
 app.use('/api/', apiRouter);
 
 // server
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(3001);
--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: process.env.API_URL || 'http://localhost:3001/api/' // Używaj ścieżki względnej lub bezwzględnej
+    baseURL: process.env.API_URL ||
+        'http://localhost:3001/api/'
 });
 
 export default instance;
