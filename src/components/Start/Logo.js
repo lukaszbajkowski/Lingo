d7b150803c1a869aa8e2d0dd33a10e996ec86002
mixvert <bajkowski1e@gmail.com>
2024-02-02 01:13:31 +0100

    Style optimization with the onset of restructuring

--- src/components/Start/Logo.js
+++ src/components/Start/Logo.js
@@ -1,21 +1,20 @@
 import logo from "../Images/logo-no-background.png";
 import {Link as RouterLink} from "react-router-dom";
 import React from "react";
+import AnimatedElement from "../Animation/AnimatedElementOnce";
 
 function Logo () {
     return (
-        <RouterLink
-            to="/"
-            style={{
-                position: 'absolute',
-                left: "0%",
-                transform: 'translate(0%, 100%)',
-                marginInline: "10%",
-                paddingInline: "40px"
-            }}
-        >
-            <img src={logo} alt="Logo" className="App-logo"/>
-        </RouterLink>
+        <div className={`logo`}>
+            <AnimatedElement className="animated-element-header animated-element animated-element-delay-200ms">
+                <RouterLink
+                    to="/"
+                    className={`logo-position`}
+                >
+                    <img src={logo} alt="Logo" className="App-logo"/>
+                </RouterLink>
+            </AnimatedElement>
+        </div>
     )
 }
 
